7760a0a986a49a8e39a7fc3c60ef333e872ab4d9
santiago <santiago.ottolini97@hotmail.com>
2021-03-31 16:29:44 -0300

    PO9: All fields are completed

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 var email = document.getElementById("email");
 var password = document.getElementById("password");
 var error = document.getElementById("error");
+var pass = document.getElementById("error");
 
 error.style.color = "red";
 
@@ -12,6 +13,10 @@ function validationLogin() {
   if (password.value === "" || password.value === null) {
     messageError.push("Complete the password");
   }
+  else{
+    messageError.push("Every validation has passed");
+  }
+  
   error.innerHTML = messageError.join(", ");
   return false;
 }
--- js/register.js
+++ js/register.js
@@ -3,6 +3,7 @@ var email = document.getElementById("email");
 var password = document.getElementById("password");
 var rPassword = document.getElementById("rPassword");
 var error = document.getElementById("error");
+var emailCheck = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 error.style.color = "red";
 
@@ -11,7 +12,7 @@ function validationRegister() {
   if (namer.value === "" || namer.value === null) {
     messageError.push("Complete the name");
   }
-  if (email.value === "" || email.value === null) {
+  if (email.value.match(emailCheck)) {
     messageError.push("Complete the email");
   }
   if (password.value === "" || password.value === null) {
@@ -19,6 +20,9 @@ function validationRegister() {
   }
   if (rPassword.value === "" || rPassword.value === null) {
     messageError.push("Complete the password");
+  }
+  else{
+    messageError.push("Every validation has passed");
   }
   error.innerHTML = messageError.join(", ");
   return false;
