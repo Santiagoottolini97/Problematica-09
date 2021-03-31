df82a38dd3bbf9539b220edd7c99bc0b6ff71523
santiago <santiago.ottolini97@hotmail.com>
2021-03-31 18:22:08 -0300

    P09: Add comments

--- js/login.js
+++ js/login.js
@@ -6,24 +6,36 @@ var pass = document.getElementById("error");
 error.style.color = "red";
 
 function validationLogin() {
+  //Create an empty array
   var messageError = [];
+  //validation email
   if (email.value === "" || email.value === null) {
+    //a new position of array was created
     messageError.push("Complete the email");
-  } else if (!isEmail(email.value)) {
+  } 
+  //if the mail not contain the necesary simbols, return a message of error
+  else if (!isEmail(email.value)) {
     messageError.push("The email is invalid");
   }
+  //validation password
   if (password.value === "" || password.value === null) {
     messageError.push("Complete the password");
   }
+  /*If the arrangement does not have any position, that is, all the 
+  fields are created because it does not have an error message that shows 
+  the message that it is correct*/
   if (messageError.length == 0) {
     messageError.push("Every validation has passed");
   }
 
+  // The positions of array separate by ,
   error.innerHTML = messageError.join(", ");
   return false;
 }
 
+//function validation mail
 function isEmail(email) {
+  //test return a boolean 
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     email
   );
--- js/register.js
+++ js/register.js
@@ -7,24 +7,36 @@ var error = document.getElementById("error");
 error.style.color = "red";
 
 function validationRegister() {
+   //Create an empty array
   var messageError = [];
+   //validation name
   if (namer.value === "" || namer.value === null) {
+    //a new position of array was created 
     messageError.push("Complete the name");
   }
+  //validation mail
   if (email.value == "" || email.value == null) {
     messageError.push("Complete the email");
-  } else if (!isEmail(email.value)) {
+  } 
+   //if the mail not contain the necesary simbols, return a message of error
+  else if (!isEmail(email.value)) {
     messageError.push("The email is not valid");
   }
+  //validation password
   if (password.value === "" || password.value === null) {
     messageError.push("Complete the password");
   }
+  //validation password
   if (rPassword.value === "" || rPassword.value === null) {
     messageError.push("Repeat your password please");
   }
+  //validation: the password has to be exactly the same
   if (password.value !== rPassword.value) {
     messageError.push("Write the same password");
   }
+  /*If the arrangement does not have any position, that is, all the 
+  fields are created because it does not have an error message that shows 
+  the message that it is correct*/
   if (messageError.length == 0) {
     messageError.push("Every validation has passed");
   }
@@ -32,6 +44,7 @@ function validationRegister() {
   return false;
 }
 
+//Validation mail
 function isEmail(email) {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     email
