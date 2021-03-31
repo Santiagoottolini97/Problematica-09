7cb000e05252d4bb83a94491d9db300e9bcab441
santiago <santiago.ottolini97@hotmail.com>
2021-03-31 16:51:28 -0300

    P09: Validation mail and repeat password

--- js/login.js
+++ js/login.js
@@ -9,14 +9,22 @@ function validationLogin() {
   var messageError = [];
   if (email.value === "" || email.value === null) {
     messageError.push("Complete the email");
+  } else if (!isEmail(email.value)) {
+    messageError.push("The email is invalid");
   }
   if (password.value === "" || password.value === null) {
     messageError.push("Complete the password");
-  }
-  else{
+  } 
+  if (messageError.length == 0) {
     messageError.push("Every validation has passed");
   }
-  
+
   error.innerHTML = messageError.join(", ");
   return false;
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
--- js/register.js
+++ js/register.js
@@ -1,4 +1,4 @@
-var namer = document.getElementById("password");
+var namer = document.getElementById("name");
 var email = document.getElementById("email");
 var password = document.getElementById("password");
 var rPassword = document.getElementById("rPassword");
@@ -12,18 +12,29 @@ function validationRegister() {
   if (namer.value === "" || namer.value === null) {
     messageError.push("Complete the name");
   }
-  if (email.value.match(emailCheck)) {
+  if (email.value == "" || email.value == null) {
     messageError.push("Complete the email");
+  } else if (!isEmail(email.value)) {
+    messageError.push("The email is not valid");
   }
   if (password.value === "" || password.value === null) {
     messageError.push("Complete the password");
   }
   if (rPassword.value === "" || rPassword.value === null) {
-    messageError.push("Complete the password");
+    messageError.push("Repeat your password please");
   }
-  else{
+  if (password.value !== rPassword.value) {
+    messageError.push("Write the same password");
+  }
+  if (messageError.length == 0) {
     messageError.push("Every validation has passed");
   }
   error.innerHTML = messageError.join(", ");
   return false;
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
